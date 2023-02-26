78c87e6a8974b20a0b97c3c7ac2b55282d62f316
Krupko Andrii <kranig15@gmail.com>
2023-02-26 22:08:01 +0200

    Initial commit project structure and setup.py

---
+++ final_project/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+	name='task_progress-app',
+	version='0.1',
+	description='A sample Python package for final project EPAM',
+	author='Andrii Krupko',
+	author_email='kranig15@gmail.com',
+	packages=['task_progress-app'],
+	install_requires=[],
+)
